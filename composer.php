9d9e0823a634dfea1e27d8e9d8b275a689555acc
Ismail SABRY <is@taztag.com>
2015-02-12 18:26:00 +0100

    update

---
+++ composer.php
@@ -0,0 +1,25 @@
+{
+    "name": "isabry/laradmin",
+    "description": "Admin integration for Laravel",
+    "keywords": ["laravel", "admin"],
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Ismail SABRY",
+            "email": "isabry@gmail.com"
+        }
+    ],
+    "require": {
+        "php": ">=5.4.0",
+        "illuminate/support": "5.0.*"
+    },
+    "autoload": {
+        "psr-4": {
+            "Isabry\\Laradmin\\": "src/"
+        },
+        "files": [
+            "src/helpers.php"
+        ]
+    },
+    "minimum-stability": "dev"
+}
