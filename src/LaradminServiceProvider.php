b5d6029ebd21cf67c2cc71851d1c79112ca180ab
Ismail SABRY <is@taztag.com>
2015-02-12 23:23:38 +0100

    update

---
+++ src/LaradminServiceProvider.php
@@ -0,0 +1,61 @@
+<?php 
+/*
+**
+*/
+namespace Isabry\Laradmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaradminServiceProvider extends ServiceProvider
+{
+	/*-------------------------------------------------------------------------
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/*-------------------------------------------------------------------------
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// Find path to the package
+		$packageFilename = with(new \ReflectionClass('\Isabry\Laradmin\LaradminServiceProvider'))->getFileName();
+		$packagePath = dirname($packageFilename);
+
+		// echo("=> Package Path: ".$packagePath."\n");
+
+		// Load the package
+		// $this->package('isabry/laradmin');
+
+		// Load views
+		$this->loadViewsFrom($packagePath.'/views', 'laradmin');
+
+		// Load routes
+		include $packagePath.'/../routes.php';
+		include $packagePath.'/../filters.php';
+	}
+
+	/*-------------------------------------------------------------------------
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+	}
+
+	/*-------------------------------------------------------------------------
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+}
